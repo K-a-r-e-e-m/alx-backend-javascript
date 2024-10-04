f5c87f77e74beb5fcdd7ea3464063c858efce527
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-04 04:56:58 +0300

    update: add new validation for inputs

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,15 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string')
+    }
+    if (typeof length !== 'number') {
+        throw new TypeError('length must be a number')
+    }
+    if (typeof students !== 'array') {
+        throw new TypeError('students must be an array')
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
