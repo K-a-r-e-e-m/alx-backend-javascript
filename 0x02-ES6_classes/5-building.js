4b75935d899390e9617ba618b88a80c18cebb1db
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-04 22:37:08 +0300

    update: add new condition to check if the subclass extend my class or not

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,27 @@
 export default class Building {
   constructor(sqft) {
-      if (new.target === Building) {
-          throw new Error('Building is an abstract class and cannot be instantiated directly.');
-      }
-      this._sqft = sqft;
-  }
-
-  // Getter for the sqft attribute
-  get sqft() {
-      return this._sqft;
-  }
-
-  // Method to be overridden by subclasses
-  evacuationWarningMessage() {
+    this._sqft = sqft;
+    // Check if the constructor is subclass not Building class
+    // and the evacuationWarningMessage is not defiend in the subclass
+    if (this.constructor !== Building
+        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+
+  // Method to be overridden in subclasses
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
