d4da15a00325a4d0021a516a0f39068a013d590b
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-05 18:15:25 +0300

    fix: update the body of promise using then

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ])
+    .then((res) => res.map((res) => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : res.reason,
+    })));
 }
