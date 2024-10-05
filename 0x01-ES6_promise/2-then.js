7ec01ff9b890ccfbef6ecb8a3b0727101f8c85d1
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-05 03:08:05 +0300

    update: fix bugs and return directly the given promise with data

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise(() => {
-    promise.then(function () {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
-    }).catch(() => {
-      return Error();
-    }).then(
-      console.log('Got a response from the API')
-    )
-  });
+    })
+    .catch(() => new Error());
 }
