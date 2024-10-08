fee9946bf3848821a43f4c0722bf507b1a3b7979
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-08 22:53:27 +0300

    Add function that return an array of dummey objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents() {
+  const ob1 = {
+    id: '1',
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const ob2 = {
+    id: '2',
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const ob3 = {
+    id: '5',
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  return [ob1, ob2, ob3];
+}
