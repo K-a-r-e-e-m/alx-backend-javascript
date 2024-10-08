cb3eca0d9d90665e379ce81d61c9d74528f902c1
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-08 22:55:18 +0300

    Add function that return an array of dummy objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,16 +1,16 @@
 export default function getListStudents() {
   const ob1 = {
-    id: '1',
+    id: 1,
     firstName: 'Guillaume',
     location: 'San Francisco',
   };
   const ob2 = {
-    id: '2',
+    id: 2,
     firstName: 'James',
     location: 'Columbia',
   };
   const ob3 = {
-    id: '5',
+    id: 5,
     firstName: 'Serena',
     location: 'San Francisco',
   };
