b380c4de9f1deb385bab88c7bb20254d7d58af06
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-10 02:09:24 +0300

    Add main file that create an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -4,3 +4,60 @@ interface Student {
   age: number;
   location: string;
 }
+
+const student1: Student = {
+  firstName: "Kareem",
+  lastName: "Hany",
+  age: 20,
+  location: "Egypt"
+} 
+
+const student2: Student = {
+  firstName: "Ahmed",
+  lastName: "Mohamed",
+  age: 32,
+  location: "USA"
+} 
+
+const studentsList: Array<Student> = [student1, student2];
+
+
+export const displayTable = (studentsList: Array<Student>): void => {
+  const table = document.createElement('table');
+  const row = document.createElement('tr');
+  table.insertAdjacentElement('beforeend', row);
+  row.insertAdjacentHTML('beforeend', '<th>firstName</th>');
+  row.insertAdjacentHTML('beforeend', '<th>lastName</th>');
+  row.insertAdjacentHTML('beforeend', '<th>age</th>');
+  row.insertAdjacentHTML('beforeend', '<th>location</th>');
+  
+  for (const student of studentsList) {
+    const dataRow = document.createElement('tr');
+    dataRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    dataRow.insertAdjacentHTML('beforeend', `<td>${student.lastName}</td>`);
+    dataRow.insertAdjacentHTML('beforeend', `<td>${student.age}</td>`);
+    dataRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+
+    table.insertAdjacentElement('beforeend', dataRow);
+  }
+  document.body.insertAdjacentElement('beforeend', table);
+}
+
+
+displayTable(studentsList);
+
+
+/* Another Solution
+const table = document.createElement("table");
+
+studentsList.forEach((student) => {
+    const row = table.insertRow();
+    const firstNameCell = row.insertCell(0);
+    const locationCell = row.insertCell(1);
+
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+});
+
+document.body.appendChild(table);
+*/
